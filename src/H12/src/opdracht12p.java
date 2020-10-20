f2c664871ecb898efc1b2aff57a5e090ea7ae1ef
TStilting <tstilting@roc-dev.com>
2020-10-20 09:55:47 +0200

    kleine update

--- src/H12/src/opdracht12p.java
+++ src/H12/src/opdracht12p.java
@@ -1,4 +1,38 @@
 package H12.src;
 
-public class opdracht12p {
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class opdracht12p extends Applet {
+
+    double[] reeks = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    Button Ok;
+    Label label;
+    String tekst;
+
+    public void init() {
+        label = new Label("Druk in voor nummers");
+        Ok = new Button("Ok");
+        Ok.addActionListener(new opdracht12p.KnopListener());
+        add(label);
+        add(Ok);
+        tekst = "";
+    }
+
+
+    public void paint(Graphics g) {
+        g.drawString(tekst, 50, 50);
+    }
+
+
+    class KnopListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            int teller = 0;
+
+
+
+        }
+    }
 }
