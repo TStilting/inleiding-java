70565175d0145227a22def84481aa5df5858d9ac
TStilting <tstilting@roc-dev.com>
2020-10-20 11:36:54 +0200

    kleine update

--- src/H12/src/opdracht12p.java
+++ src/H12/src/opdracht12p.java
@@ -7,29 +7,41 @@ import java.awt.event.ActionListener;
 
 public class opdracht12p extends Applet {
 
-    double[] reeks = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    //String[] reeks;
+    TextField tekstvak;
     Button Ok;
     Label label;
-    String tekst;
+    String[] tekst;
 
     public void init() {
-        label = new Label("Druk in voor nummers");
+        tekst = new String [10];
+        tekstvak = new TextField("", 15);
+        label = new Label("voer naam en nummer in");
         Ok = new Button("Ok");
         Ok.addActionListener(new opdracht12p.KnopListener());
         add(label);
+        add(tekstvak);
         add(Ok);
-        tekst = "";
     }
 
 
     public void paint(Graphics g) {
-        g.drawString(tekst, 50, 50);
+        g.drawString(tekst[1], 50, 50);
+        g.drawString(tekst[2], 50, 60);
+        g.drawString(tekst[3], 50, 70);
+        g.drawString(tekst[4], 50, 80);
+        g.drawString(tekst[5], 50, 90);
+        g.drawString(tekst[6], 50, 100);
+        g.drawString(tekst[7], 50, 110);
+        g.drawString(tekst[8], 50, 120);
+        g.drawString(tekst[9], 50, 130);
+        g.drawString(tekst[10], 50, 140);
     }
 
 
     class KnopListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            int teller = 0;
+
 
 
 
