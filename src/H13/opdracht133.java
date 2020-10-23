2c35bce71fdfe4123d98b6eb8308a9ffcc52b75a
TStilting <tstilting@roc-dev.com>
2020-10-23 11:48:59 +0200

    13.3 geprobeerd een knop te laten werken

--- src/H13/opdracht133.java
+++ src/H13/opdracht133.java
@@ -12,8 +12,13 @@ public class opdracht133 extends Applet {
     double[] beton1;
     double[] beton2;
     Button baksteen, beton;
-    boolean uitvoerSteen;
-    boolean uitvoerBeton;
+    int w = 50;
+    int h = 30;
+    int x = 50;
+    int y = 50;
+    int w2 = 60;
+    int h2 = 40;
+    int x2 = 350;
 
 
     public void init() {
@@ -25,95 +30,77 @@ public class opdracht133 extends Applet {
         beton = new Button("Beton");
         baksteen.addActionListener(new KnopListener());
         beton.addActionListener(new KnopListener1());
-        uitvoerSteen = false;
-        uitvoerBeton = false;
         add(baksteen);
         add(beton);
     }
 
-    public void paint(Graphics g) {
-        int w = 50;
-        int h = 30;
-        int x = 50;
-        int y = 50;
-        int w2 = 60;
-        int h2 = 40;
-        int x2 = 350;
-
-        if(uitvoerSteen == true) {
-            for (int teller = 0; teller < steen1.length; teller++) {
-                tekenSteen (g, x, y, w, h);
-                x += 50;
-            }
-            x = 75;
-            y += 30;
-            for (int teller = 0; teller < steen2.length; teller++) {
-                tekenSteen (g, x, y, w, h);
-                x += 50;
-            }
-            x = 50;
-            y += 30;
-            for (int teller = 0; teller < steen1.length; teller++) {
-                tekenSteen (g, x, y, w, h);
-                x += 50;
-            }
-            x = 75;
-            y += 30;
-            for (int teller = 0; teller < steen2.length; teller++) {
-                tekenSteen (g, x, y, w, h);
-                x += 50;
-            }
-            x = 50;
-            y += 30;
-            for (int teller = 0; teller < steen1.length; teller++) {
-                tekenSteen (g, x, y, w, h);
-                x += 50;
-            }
-        }
-
-        if(uitvoerBeton == true) {
-            for (int teller = 0; teller < beton1.length; teller++) {
-                tekenBeton (g, x2, y, w2, h2);
-                x += 50;
-            }
-            x = 75;
-            y += 30;
-            for (int teller = 0; teller < beton2.length; teller++) {
-                tekenBeton (g, x2, y, w2, h2);
-                x += 50;
-            }
-            x = 50;
-            y += 30;
-            for (int teller = 0; teller < beton1.length; teller++) {
-                tekenBeton (g, x2, y, w2, h2);
-                x += 50;
-            }
-            x = 75;
-            y += 30;
-            for (int teller = 0; teller < beton2.length; teller++) {
-                tekenBeton (g, x2, y, w2, h2);
-                x += 50;
-            }
-            x = 50;
-            y += 30;
-            for (int teller = 0; teller < beton1.length; teller++) {
-                tekenBeton (g, x2, y, w2, h2);
-                x += 50;
-            }
-        }
-    }
+    public void paint(Graphics g) {}
 
     class KnopListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            uitvoerSteen = true;
-            uitvoerBeton = false;
+            Graphics g = null;
+            for (int teller = 0; teller < steen1.length; teller++) {
+                tekenSteen (g, x, y, w, h);
+                x += 50;
+            }
+            x = 75;
+            y += 30;
+            for (int teller = 0; teller < steen2.length; teller++) {
+                tekenSteen (g, x, y, w, h);
+                x += 50;
+            }
+            x = 50;
+            y += 30;
+            for (int teller = 0; teller < steen1.length; teller++) {
+                tekenSteen (g, x, y, w, h);
+                x += 50;
+            }
+            x = 75;
+            y += 30;
+            for (int teller = 0; teller < steen2.length; teller++) {
+                tekenSteen (g, x, y, w, h);
+                x += 50;
+            }
+            x = 50;
+            y += 30;
+            for (int teller = 0; teller < steen1.length; teller++) {
+                tekenSteen (g, x, y, w, h);
+                x += 50;
+            }
         }
     }
 
     class KnopListener1 implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            uitvoerBeton = true;
-            uitvoerSteen = false;
+            Graphics g = null;
+            for (int teller1 = 0; teller1 < beton1.length; teller1++) {
+                tekenBeton (g, x2, y, w2, h2);
+                x += 50;
+            }
+            x = 75;
+            y += 30;
+            for (int teller1 = 0; teller1 < beton2.length; teller1++) {
+                tekenBeton (g, x2, y, w2, h2);
+                x += 50;
+            }
+            x = 50;
+            y += 30;
+            for (int teller1 = 0; teller1 < beton1.length; teller1++) {
+                tekenBeton (g, x2, y, w2, h2);
+                x += 50;
+            }
+            x = 75;
+            y += 30;
+            for (int teller1 = 0; teller1 < beton2.length; teller1++) {
+                tekenBeton (g, x2, y, w2, h2);
+                x += 50;
+            }
+            x = 50;
+            y += 30;
+            for (int teller1 = 0; teller1 < beton1.length; teller1++) {
+                tekenBeton (g, x2, y, w2, h2);
+                x += 50;
+            }
         }
     }
 
