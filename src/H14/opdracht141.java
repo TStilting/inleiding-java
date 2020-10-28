d2922eb88fe2ae4e699d55bcbd4f4e7813d8d407
TStilting <tstilting@roc-dev.com>
2020-10-28 12:07:05 +0100

    H14 aan gewerkt

--- src/H14/opdracht141.java
+++ src/H14/opdracht141.java
@@ -1,10 +1,29 @@
 package H14;
 
 import java.awt.*;
+import java.applet.*;
 
-public class opdracht141 {
+public class opdracht141 extends Applet {
 
-    public void init() {}
+    double r;
+    String kaart [];
+    String kleur [];
 
-    public void paint(Graphics g) {}
+    public void init() {
+        r = Math.random();
+        kaart = new String[13];
+        kleur = new String[4];
+        kaart[1] = "A";
+        kaart[11] = "J";
+        kaart[12] = "Q";
+        kaart[13] = "K";
+        kleur[1] = "schoppen";
+        kleur[2] = "harten";
+        kleur[3] = "klaver";
+        kleur[4] = "ruiten";
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("" + kleur + " " + kaart, 50, 60 );
+    }
 }
