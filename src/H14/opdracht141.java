84b61061e04e8ef35cfa3f238cab18be3902cdd4
TStilting <tstilting@roc-dev.com>
2020-10-28 13:42:52 +0100

    H14 verder gewerkt

--- src/H14/opdracht141.java
+++ src/H14/opdracht141.java
@@ -6,14 +6,23 @@ import java.applet.*;
 public class opdracht141 extends Applet {
 
     double r;
-    String kaart [];
-    String kleur [];
+    String[] kaart;
+    String[] kleur;
 
     public void init() {
         r = Math.random();
-        kaart = new String[13];
-        kleur = new String[4];
+        kaart = new String[14];
+        kleur = new String[5];
         kaart[1] = "A";
+        kaart[2] = "2";
+        kaart[3] = "3";
+        kaart[4] = "4";
+        kaart[5] = "5";
+        kaart[6] = "6";
+        kaart[7] = "7";
+        kaart[8] = "8";
+        kaart[9] = "9";
+        kaart[10] = "10";
         kaart[11] = "J";
         kaart[12] = "Q";
         kaart[13] = "K";
@@ -21,9 +30,11 @@ public class opdracht141 extends Applet {
         kleur[2] = "harten";
         kleur[3] = "klaver";
         kleur[4] = "ruiten";
+        kleur[(int) r] = String.valueOf((int)(r * 4 + 1));
+        kaart[(int) r] = String.valueOf((int)(r * 13 + 1));
     }
 
     public void paint(Graphics g) {
-        g.drawString("" + kleur + " " + kaart, 50, 60 );
+        g.drawString( kleur[(int) r] + " " + kaart[(int) r], 50, 60 );
     }
 }
