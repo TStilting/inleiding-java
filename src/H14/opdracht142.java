aa4fe213825df0bd07fe6eb532b40ddefc3375de
TStilting <tstilting@roc-dev.com>
2020-10-29 13:44:15 +0100

    14.2 aangemaakt

---
+++ src/H14/opdracht142.java
@@ -0,0 +1,54 @@
+package H14;
+
+import java.awt.*;
+import java.applet.*;
+
+public class opdracht142 extends Applet {
+
+    double r;
+    String[] kaart;
+    String[] kleur;
+
+    public void init() {
+        r = Math.random();
+        kaart = new String[14];
+        kleur = new String[5];
+        kleur[(int) r] = String.valueOf((int)(r * 4 + 1));
+        kaart[(int) r] = String.valueOf((int)(r * 13 + 1));
+    }
+
+
+    public void paint(Graphics g) {
+        if (kaart[(int) r].equals(String.valueOf(1))) {
+            kaart[(int) r] = "A";
+        }
+
+        if (kaart[(int) r].equals(String.valueOf(11))) {
+            kaart[(int) r] = "J";
+        }
+
+        if (kaart[(int) r].equals(String.valueOf(12))) {
+            kaart[(int) r] = "Q";
+        }
+
+        if (kaart[(int) r].equals(String.valueOf(13))) {
+            kaart[(int) r] = "K";
+        }
+
+        if (kleur[(int) r].equals(String.valueOf(1))) {
+            kleur[(int) r] = "schoppen";
+        }
+
+        if (kleur[(int) r].equals(String.valueOf(2))) {
+            kleur[(int) r] = "klaver";
+        }
+
+        if (kleur[(int) r].equals(String.valueOf(3))) {
+            kleur[(int) r] = "harten";
+        }
+
+        if (kleur[(int) r].equals(String.valueOf(4))) {
+            kleur[(int) r] = "ruiten";
+        }
+    }
+}
